6b33ac90b8a20b2089f4f2da05777c7237f800bf
Arseniy Olevskiy <arsidian@gmail.com>
2024-05-15 21:39:40 +0500

    calculator changes commit

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -2,82 +2,111 @@
 'use strict'
 
 let switchTheme = null;
+let theme = 'light';
+if (localStorage.getItem('theme') === 'dark' || (localStorage.getItem('theme') === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) theme = 'dark';
+const colors = {
+	light: {
+		purple: '#A78BFA',
+		yellow: '#FBBF24',
+		sky: '#7DD3FC',
+		blue: '#1D4ED8',
+		textColor: '#6B7280',
+		yellowGradientStart: 'rgba(250, 219, 139, 0.33)',
+		purpleGradientStart: 'rgba(104, 56, 248, 0.16)',
+		skyGradientStart: 'rgba(56, 187, 248, 0.16)',
+		tealGradientStart: 'rgba(56, 248, 222, 0.16)',
+		yellowGradientStop: 'rgba(250, 219, 139, 0)',
+		purpleGradientStop: 'rgba(104, 56, 248, 0)',
+		gridColor: '#DBEAFE',
+		tooltipBackground: '#fff',
+		fractionColor: '#EDE9FE',
+	},
+	dark: {
+		purple: '#7C3AED',
+		yellow: '#D97706',
+		sky: '#0284C7',
+		blue: '#101E47',
+		textColor: '#fff',
+		yellowGradientStart: 'rgba(146, 123, 67, 0.23)',
+		purpleGradientStart: 'rgba(78, 55, 144, 0.11)',
+		skyGradientStart: 'rgba(56, 187, 248, 0.16)',
+		tealGradientStart: 'rgba(56, 248, 222, 0.16)',
+		yellowGradientStop: 'rgba(250, 219, 139, 0)',
+		purpleGradientStop: 'rgba(104, 56, 248, 0)',
+		gridColor: '#162B64',
+		tooltipBackground: '#1C3782',
+		fractionColor: '#41467D',
+	},
+};
 
-import("./assets/js/lib/chartjs/chart.js").then((e) => {
-	let Chart = e.Chart
-	let registerables = e.registerables
-	Chart.register(...registerables)
+let data = [
+	{
+		data: [28, 66, 6],
+		labels: ['28%', '66%', '6%'],
+		backgroundColor: [colors[theme].yellow, colors[theme].purple, colors[theme].sky],
+		borderColor: '#DDD6FE',
+		borderWidth: 0,
+	},
+];
 
-	const theme = localStorage.getItem('theme') !== 'system' ? localStorage.getItem('theme') : window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-	const colors = {
-		light: {
-			purple: '#A78BFA',
-			yellow: '#FBBF24',
-			sky: '#7DD3FC',
-			blue: '#1D4ED8',
-			textColor: '#6B7280',
-			yellowGradientStart: 'rgba(250, 219, 139, 0.33)',
-			purpleGradientStart: 'rgba(104, 56, 248, 0.16)',
-			skyGradientStart: 'rgba(56, 187, 248, 0.16)',
-			tealGradientStart: 'rgba(56, 248, 222, 0.16)',
-			yellowGradientStop: 'rgba(250, 219, 139, 0)',
-			purpleGradientStop: 'rgba(104, 56, 248, 0)',
-			gridColor: '#DBEAFE',
-			tooltipBackground: '#fff',
-			fractionColor: '#EDE9FE',
+let options = {
+	rotation: 0,
+	cutout: '37%',
+	hover: {mode: null},
+	responsive: false,
+	layout: {
+		padding: 30,
+	},
+	plugins: {
+		tooltip: {
+			enabled: false,
 		},
-		dark: {
-			purple: '#7C3AED',
-			yellow: '#D97706',
-			sky: '#0284C7',
-			blue: '#101E47',
-			textColor: '#fff',
-			yellowGradientStart: 'rgba(146, 123, 67, 0.23)',
-			purpleGradientStart: 'rgba(78, 55, 144, 0.11)',
-			skyGradientStart: 'rgba(56, 187, 248, 0.16)',
-			tealGradientStart: 'rgba(56, 248, 222, 0.16)',
-			yellowGradientStop: 'rgba(250, 219, 139, 0)',
-			purpleGradientStop: 'rgba(104, 56, 248, 0)',
-			gridColor: '#162B64',
-			tooltipBackground: '#1C3782',
-			fractionColor: '#41467D',
+		legend: {
+			display: false,
 		},
-	};
+	},
+};
 
-	let data = [
-		{
-			data: [28, 66, 6],
-			labels: ['28%', '66%', '6%'],
-			backgroundColor: [colors[theme].yellow, colors[theme].purple, colors[theme].sky],
-			borderColor: '#DDD6FE',
-			borderWidth: 0,
-		},
-	];
+const customDataLabels = {
+	id: 'customDataLabel',
+	afterDatasetDraw(chart, args, pluginOptions) {
+		const {
+			ctx,
+			data
+		} = chart;
+		ctx.save();
 
-	let options = {
-		rotation: 0,
-		cutout: '37%',
-		hover: {mode: null},
-		responsive: false,
-		layout: {
-			padding: 30,
-		},
-		plugins: {
-			tooltip: {
-				enabled: false,
-			},
-			legend: {
-				display: false,
-			},
-		},
-	};
+		data.datasets[0].data.forEach((datapoint, index) => {
+			const { x, y } = chart.getDatasetMeta(0).data[index].tooltipPosition();
+			ctx.textAlign = 'center';
+			ctx.font = '14px Inter';
+			ctx.fillStyle = '#fff';
+			ctx.textBaseline = 'middle';
+			let toolTipText = datapoint != '0' ? datapoint + '%' : '';
+			ctx.fillText(toolTipText, x, y);
+		});
+	},
+};
+
+let donutBig = new Chart(document.getElementById('chartDonutBig'), {
+	type: 'doughnut',
+	data: {
+		datasets: data,
+	},
+	options: options,
+	plugins: [customDataLabels],
+});
+
+switchTheme = function(theme) {
+	donutBig.destroy()
 
 	const customDataLabels = {
 		id: 'customDataLabel',
 		afterDatasetDraw(chart, args, pluginOptions) {
 			const {
 				ctx,
-				data
+				data,
+				chartArea: { top, bottom, left, right, width, height },
 			} = chart;
 			ctx.save();
 
@@ -93,7 +122,7 @@ import("./assets/js/lib/chartjs/chart.js").then((e) => {
 		},
 	};
 
-	let donutBig = new Chart(document.getElementById('chartDonutBig'), {
+	donutBig = new Chart(document.getElementById('chartDonutBig'), {
 		type: 'doughnut',
 		data: {
 			datasets: data,
@@ -102,47 +131,12 @@ import("./assets/js/lib/chartjs/chart.js").then((e) => {
 		plugins: [customDataLabels],
 	});
 
-	switchTheme = function(theme) {
-		donutBig.destroy()
+	donutBig.data.datasets[0].backgroundColor = [colors[theme].yellow, colors[theme].purple, colors[theme].sky];
+	donutBig.update()
+}
 
-		const customDataLabels = {
-			id: 'customDataLabel',
-			afterDatasetDraw(chart, args, pluginOptions) {
-				const {
-					ctx,
-					data,
-					chartArea: { top, bottom, left, right, width, height },
-				} = chart;
-				ctx.save();
-
-				data.datasets[0].data.forEach((datapoint, index) => {
-					const { x, y } = chart.getDatasetMeta(0).data[index].tooltipPosition();
-					ctx.textAlign = 'center';
-					ctx.font = '14px Inter';
-					ctx.fillStyle = '#fff';
-					ctx.textBaseline = 'middle';
-					let toolTipText = datapoint != '0' ? datapoint + '%' : '';
-					ctx.fillText(toolTipText, x, y);
-				});
-			},
-		};
-
-		donutBig = new Chart(document.getElementById('chartDonutBig'), {
-			type: 'doughnut',
-			data: {
-				datasets: data,
-			},
-			options: options,
-			plugins: [customDataLabels],
-		});
-
-		donutBig.data.datasets[0].backgroundColor = [colors[theme].yellow, colors[theme].purple, colors[theme].sky];
-		donutBig.update()
-	}
-
-	window.changeChartData = function(values) {
-		donutBig.data.datasets[0].data = values
-		donutBig.data.datasets[0].labels = values.map(value => `${value}%`)
-		donutBig.update()
-	}
-})
+window.changeChartData = function(values) {
+	donutBig.data.datasets[0].data = values
+	donutBig.data.datasets[0].labels = values.map(value => `${value}%`)
+	donutBig.update()
+}
